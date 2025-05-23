4d24e507ea1fa4f8d7888ee64e6315a26a1e424c
Diego Luis de Aquino Leite <diego.leite@picpay.com>
2025-05-23 10:16:37 -0300

    fix

--- generate_and_apply_pipeline.py
+++ generate_and_apply_pipeline.py
@@ -40,42 +40,38 @@ def main():
 
     # 1. Geração e Aplicação das Tasks Tekton separadas
     print(f"Gerando e aplicando Tasks separadas em '{GENERATED_TASKS_DIR}'...")
-    for step_config in config['steps']:
+    for step_config in config['task_steps']:
         task_tekton_name = normalize_name(step_config['name'])
         generated_pipeline_tasks_info.append({'name': task_tekton_name, 'runAfter': step_config.get('runAfter')})
 
-        # Define a estrutura da Task Tekton
+        # Concatena todos os comandos em um único script
+        all_commands = '\n'.join(step_config['commands'])
+        script_content = f"""#!/bin/sh
+apt-get update && apt-get install -y make || echo 'make already installed or could not install'
+{all_commands}
+"""
+
+        # Define o único step para a Task Tekton
+        step_spec = {
+            'name': task_tekton_name,
+            'image': step_config['image'],
+            'script': script_content,
+            'workingDir': '$(workspaces.shared-workspace.path)'
+        }
+
+        if 'environment' in step_config:
+            env_vars = [{'name': k, 'value': v} for k, v in step_config['environment'].items()]
+            step_spec['env'] = env_vars
+
         task_tekton = {
             'apiVersion': 'tekton.dev/v1',
             'kind': 'Task',
             'metadata': {'name': task_tekton_name, 'namespace': NAMESPACE},
             'spec': {
                 'workspaces': [{'name': 'shared-workspace'}],
-                'steps': []
+                'steps': [step_spec]  # Apenas um step por task
             }
         }
-
-        # Itera sobre os comandos dentro de cada passo para criar os Steps da Task
-        for idx, command in enumerate(step_config['commands']):
-            step_name = f"step-{idx+1}"
-
-            script_content = f"""#!/bin/sh
-apt-get update && apt-get install -y make || echo 'make already installed or could not install'
-{command}
-"""
-
-            step_spec = {
-                'name': normalize_name(step_name),
-                'image': step_config['image'],
-                'script': script_content,
-                'workingDir': '$(workspaces.shared-workspace.path)'
-            }
-
-            if 'environment' in step_config:
-                env_vars = [{'name': k, 'value': v} for k, v in step_config['environment'].items()]
-                step_spec['env'] = env_vars
-
-            task_tekton['spec']['steps'].append(step_spec)
 
         task_output_path = os.path.join(GENERATED_TASKS_DIR, f"{task_tekton_name}-task.yaml")
         with open(task_output_path, 'w') as f:
